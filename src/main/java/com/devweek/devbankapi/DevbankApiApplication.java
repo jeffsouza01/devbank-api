85639d2f0c24cf74d6f7a76b849467bbfe12e84e
Jefferson da Silva <je_jss@hotmail.com>
2022-05-05 09:47:33 -0300

    innitial commit

---
+++ src/main/java/com/devweek/devbankapi/DevbankApiApplication.java
@@ -0,0 +1,13 @@
+package com.devweek.devbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevbankApiApplication.class, args);
+	}
+
+}
